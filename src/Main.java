5c51ac3535dd9253d18322f19202e3bd27ecc7c3
Trey <burlints@mail.uc.edu>
2022-11-15 15:31:20 -0500

    Progress

--- src/Main.java
+++ src/Main.java
@@ -8,23 +8,34 @@ public class Main {
         Random rnd = new Random();
 
         boolean done = false;
+        boolean anotherYN = false;
+        String choiceYN = "";
+        String trash = "";
         int die1 = rnd.nextInt(6) + 1;
         int die2 = rnd.nextInt(6) + 1;
         int crapsRoll = die1 + die2;
 
+
+        System.out.println("Your roll is " + crapsRoll + ". ");
         do
         {
-            System.out.print("Press any key to roll the die. ");
-            crapsRoll = in.nextInt();
-            while(crapsRoll >=1, crapsRoll <= 12)
+            if(crapsRoll == 2 || crapsRoll == 3 || crapsRoll == 12)
             {
+                System.out.println("Since you rolled a " + crapsRoll + ", you crapped out, losing the game! ");
+                done = true;
 
             }
+            else if(crapsRoll == 7 || crapsRoll == 11)
+            {
+                System.out.println("Since you rolled a " + crapsRoll + ", you have gotten a natural, winning the game! ");
+                done = true;
+            }
+            else
+            {
+                System.out.println("Since you rolled a " + crapsRoll + ", you must keep rolling");
 
-
+            }
         }while(!done);
-
-
 
     }
 }
