5cb5beeae0349e5e3b981cfdc4e3deae8168911d
Trey <burlints@mail.uc.edu>
2022-11-17 21:52:14 -0500

    Finished

--- src/Main.java
+++ src/Main.java
@@ -6,36 +6,63 @@ public class Main {
     {
         Scanner in = new Scanner(System.in);
         Random rnd = new Random();
+        Random newRnd = new Random();
 
         boolean done = false;
-        boolean anotherYN = false;
+        String anotherYN = "";
         String choiceYN = "";
         String trash = "";
         int die1 = rnd.nextInt(6) + 1;
         int die2 = rnd.nextInt(6) + 1;
-        int crapsRoll = die1 + die2;
+        int crapsRoll = 0;
 
-
-        System.out.println("Your roll is " + crapsRoll + ". ");
         do
         {
+            die1 = rnd.nextInt(6) + 1;
+            die2 = rnd.nextInt(6) + 1;
+            crapsRoll = die1 + die2;
+            // Initial roll
+            System.out.println("Your first roll is " + crapsRoll + ". ");
+            // If the user rolls a winning number
             if(crapsRoll == 2 || crapsRoll == 3 || crapsRoll == 12)
             {
-                System.out.println("Since you rolled a " + crapsRoll + ", you crapped out, losing the game! ");
-                done = true;
-
+                System.out.println("Since you rolled " + crapsRoll + ", you crapped out, losing the game! ");
             }
+            // If the user rolls a losing number
             else if(crapsRoll == 7 || crapsRoll == 11)
             {
-                System.out.println("Since you rolled a " + crapsRoll + ", you have gotten a natural, winning the game! ");
-                done = true;
+                System.out.println("Since you rolled " + crapsRoll + ", you have gotten a natural, winning the game! ");
             }
+            // If the user rolls a neutral number
             else
             {
-                System.out.println("Since you rolled a " + crapsRoll + ", you must keep rolling");
+                int newRoll = 0;
+                // Loop for making new rolls
+                while(newRoll != crapsRoll && newRoll != 7)
+                {
+                    die1 = rnd.nextInt(6) + 1;
+                    die2 = rnd.nextInt(6) + 1;
+                    newRoll = die1 + die2;
+                    System.out.println("Your next roll is " + newRoll);
+                }
+                // If the user rolls the point sum
+                if(newRoll == crapsRoll)
+                {
+                    System.out.println("You have rolled the point sum, you win! ");
+                }
+                // If the user rolls a 7
+                else
+                {
+                    System.out.println("You have rolled a 7, you lost! ");
+                }
+            }
 
+            System.out.print("Would you like to play again? [Y/N] ");
+            anotherYN = in.nextLine();
+            if(anotherYN.equalsIgnoreCase("N"))
+            {
+                done = true;
             }
         }while(!done);
-
     }
 }
